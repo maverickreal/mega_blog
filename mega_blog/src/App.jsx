7286b64757407b9ecce48ed09b0b9855c259fc35
Aditya Saxena <adity09@outlook.com>
2025-02-22 16:23:43 +0530

    service added

---
+++ mega_blog/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+
+    return (
+        <>
+        <h1>Mega Blog</h1>
+        </>
+    );
+};
+
+export default App;
---
+++ mega_blog/src/appwrite/config.js
@@ -0,0 +1,133 @@
+import config from "../conf/config";
+import { Client, ID, Database, Storage, Query } from "appwrite";
+
+export class Service {
+    client;
+    database;
+    bucket;
+
+    constructor() {
+        this.client = new Client();
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+
+        this.database = new Database(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost(title, slug, content, featuredImage, status, userId) {
+        try {
+            return await this.database.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePost(title, slug, content, featuredImage, status) {
+        try {
+            return await this.database.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.database.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.database.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getPosts() {
+        try {
+            const queries = [
+                Query.equal("status", "active")
+            ];
+
+            return await this.database.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async dleteFile(id) {
+        try {
+            return await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                id
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    getFilePreview(id) {
+        try {
+            return this.bucket.getFilePreview(
+                config.appwriteBucketId,
+                id
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+};
+
+const service = new Service();
+
+export default service;
