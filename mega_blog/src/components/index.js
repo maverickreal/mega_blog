77278ec3840ccecefd76a9d76627c0799bcc6cd0
Aditya Saxena <adity09@outlook.com>
2025-04-16 00:14:38 +0530

    added postForm

--- mega_blog/src/components/index.js
+++ mega_blog/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./footer/Footer.jsx";
 import Container from "./container/Container.jsx";
 import Logo from "./logo/Logo.jsx";
 import LogoutBtn from "./header/LogoutBtn.jsx";
+import RTE from "./rte/RTE.jsx";
 
-export {Header, Footer, Container, Logo, LogoutBtn};
+export {Header, Footer, Container, Logo, LogoutBtn, RTE};
